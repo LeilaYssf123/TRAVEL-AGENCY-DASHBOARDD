e6a02d7eee0421932bcb2e8b0781e781f2d251e4
Leila <leila.yousfi@esprit.tn>
2025-09-04 08:37:33 +0100

    GENERATE

---
+++ app/routes/admin/trips.tsx
@@ -0,0 +1,15 @@
+import {Header} from "../../../components";
+
+const Trips = () => {
+    return (
+        <main className="All users wrapper">
+            <Header
+                title="Trips"
+                description="View ans edit AI-generated travel plans "
+                ctaText="Create a Trip"
+                ctaUrl="/trips/create"
+            />
+        </main>
+    )
+}
+export default Trips
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
-import {useLocation} from "react-router";
+import {Link, useLocation} from "react-router";
 import {cn} from "~/lib/utils";
+import {ButtonComponent} from "@syncfusion/ej2-react-buttons";
 
 interface Props {
     title:string;
     description:string;
+    ctaText?:string;
+    ctaUrl?: string;
 }
-const Header = ({title, description}:Props) => {
+const Header = ({title, description, ctaText,ctaUrl}:Props) => {
     const location=useLocation();
     return (
         <header className="header">
@@ -19,6 +22,15 @@ const Header = ({title, description}:Props) => {
                     'text-sm md:text-lg ')}>{description}</p>
 
         </article>
+
+            {ctaUrl && ctaUrl && (
+                <Link to={ctaUrl}>
+                    <ButtonComponent type="button" className="button-class !h-11 !h-11 !w-full md:w-[240px]">
+                        <img src="/assets/icons/plus.svg" alt="plus" className="size-5" />
+                           <span className="p-16-semibold text-white">{ctaText}</span>
+                    </ButtonComponent>
+                        </Link>
+            )}
         </header>
 
     )
